825798a69856e650f4acd0570d3d75e27ff5a9ad
Fikri Rudiansyah <fortuneswap@gmail.com>
2021-07-16 14:54:02 +0700

    Change color theme

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -27,7 +27,7 @@ const StyledSVG = styled.svg<{ size: string; stroke?: string }>`
 export default function Loader() {
 
   return (
-    <StyledSVG viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" size="32px" stroke="red">
+    <StyledSVG viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" size="32px" stroke="yellow">
       <path
         d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 9.27455 20.9097 6.80375 19.1414 5"
         strokeWidth="2.5"
